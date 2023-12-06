9a05713ff2d2f4a39d76599f79a3886fe5ba6f09
Ayzirekjolkyzy <152137593+Ayzirekjolkyzy@users.noreply.github.com>
2023-12-06 21:20:24 +0600

    Time: 2 ms (49.74%), Space: 6.5 MB (6.05%) - LeetHub

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int totalMoney(int n) {
+        int ret=0, monday=1;
+ while(n>0) {
+     for(int day=0; day<min(n, 7); day++)
+     { ret+=monday+day;
+      }
+     n-=7; monday++;
+ } return ret;
+ }
+};
