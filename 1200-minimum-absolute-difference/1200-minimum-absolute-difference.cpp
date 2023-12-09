d575302f9d611d5c1e72c132a79984585e9bdb1b
Ayzirekjolkyzy <152137593+Ayzirekjolkyzy@users.noreply.github.com>
2023-12-09 22:15:54 +0600

    Time: 44 ms (96.42%), Space: 32.6 MB (38.72%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -3,8 +3,7 @@ public:
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
         sort(arr.begin(), arr.end()); int mindif=arr[1]-arr[0];
         for(int i=1; i<arr.size(); i++) {
-            if(mindif>(arr[i]-arr[i-1])) mindif=arr[i]-arr[i-1];
-        }
+           mindif = min(mindif, arr[i]-arr[i-1]);         }
         vector<vector<int>> ret;
         for(int i=1; i<arr.size(); i++) {
             if((arr[i]-arr[i-1])==mindif) ret.push_back({arr[i-1], arr[i]});
