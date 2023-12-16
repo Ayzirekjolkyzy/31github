8fe539922895aa318f1bc45b422063f02b07a47d
Ayzirekjolkyzy <152137593+Ayzirekjolkyzy@users.noreply.github.com>
2023-12-16 20:30:25 +0600

    Time: 3 ms (62.08%), Space: 7.1 MB (53.14%) - LeetHub

--- 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.cpp
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int secondHighest(string s) {
-        set<int, greater<int>> temp;
+        set<int> temp;
         for(int i=0; i<s.size(); i++) {
             if(isdigit(s[i])) {
                 temp.insert(int(s[i])-48);
             }
         }
         if(temp.size()<2) return -1;
-         set <int> :: iterator it = temp.begin(); it++;
+         set <int> :: iterator it = temp.end(); 
+        --it; it--;
         return *it;
     }
 };
