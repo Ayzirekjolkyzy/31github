f33bee72754c8effd9f778d56bd5088f41b6de29
Ayzirekjolkyzy <152137593+Ayzirekjolkyzy@users.noreply.github.com>
2023-12-17 21:30:38 +0600

    Time: 5 ms (49.77%), Space: 7.7 MB (88.94%) - LeetHub

--- 1417-reformat-the-string/1417-reformat-the-string.cpp
+++ 1417-reformat-the-string/1417-reformat-the-string.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     string reformat(string s) {
-        string temp="", ret=""; int i=0;
-        for(i=0; i<s.size(); i++) {
-            if(isdigit(s[i])) { temp+=s[i]; s.erase(s.begin()+i); i--;}
-        }
-        
-        if(s.size()>temp.size())     {if((s.size()-temp.size())>1) return "";}
-        else if(s.size()<temp.size())     {if((temp.size()-s.size())>1) return "";}
-        i=0;
-        while(i<s.size()&&i<temp.size()) {
-           if(s.size()>temp.size()) {ret+=s[i]; ret+=temp[i];}
-            else {ret+=temp[i]; ret+=s[i]; }
-            i++; }
-        if(s.size()>temp.size()) ret+=s[i]; 
-        else if(s.size()<temp.size()) ret+=temp[i];
-        return ret;
+        string a="",d="";
+        for(auto x:s)
+            isalpha(x)?a.push_back(x):d.push_back(x);
+            if(abs(int(a.size()-d.size()))>1)
+            return "";
+        bool alpha=a.size()>d.size();
+        int i=0,j=0,k=0;
+            while(i<s.size()){
+                if(alpha){
+                    s[i++]=a[j++];
+                }
+                else{
+                    s[i++]=d[k++];
+                }
+                alpha=!alpha;
+            }
+        return s;
     }
 };
