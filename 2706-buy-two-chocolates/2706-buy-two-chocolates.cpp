2efc6416f895e5a23b9e0313ab74574169d9bab6
Ayzirekjolkyzy <152137593+Ayzirekjolkyzy@users.noreply.github.com>
2023-12-20 22:48:33 +0600

    Time: 19 ms (19.09%), Space: 46.9 MB (94.77%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        sort(prices.begin(), prices.end());
+        if(money<(prices[0]+prices[1])) {
+            return money;
+        }
+        return money-prices[0]-prices[1];
+    }
+};
