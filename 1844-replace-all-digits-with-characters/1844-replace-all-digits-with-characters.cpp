77864bff36d464c9312c5881d19388d40d20e721
Ayzirekjolkyzy <152137593+Ayzirekjolkyzy@users.noreply.github.com>
2023-12-23 00:50:56 +0600

    Time: 3 ms (45.84%), Space: 6.5 MB (87.80%) - LeetHub

--- 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.cpp
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     string replaceDigits(string s) {
-        for(int i=0; i<s.size(); i++) {
-            if(!isalpha(s[i])) s[i]=(s[i-1]+s[i]-'0');
+        for(int i=1; i<s.size(); i+=2) {
+           s[i]+=s[i-1]-'0';
         }
         return s;
     }
