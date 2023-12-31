8f2ad9a9035e3c2f572d62739b3695f12bf10c6b
Ayzirekjolkyzy <152137593+Ayzirekjolkyzy@users.noreply.github.com>
2023-12-31 22:29:25 +0600

    Time: 11 ms (9.06%), Space: 11.3 MB (6.42%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int minimumCost(vector<int>& cost) {
         int sum=0;
-        sort(cost.begin(), cost.end(), greater<int>());
-        int i=0, t=0;
-        while(i<cost.size()){
-            if(t==2) {t=0; i++;}
+        sort(cost.begin(), cost.end());
+        int i=cost.size()-1, t=0;
+        while(i>=0){
+            if(t==2) {t=0; i--;}
             else{
             sum+=cost[i];
-            i++; t++; }
+            i--; t++; }
         }
         return sum;
         
